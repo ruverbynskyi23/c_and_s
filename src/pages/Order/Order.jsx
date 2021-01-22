b946f21bb2955e8ecf9ae653c409c518d5762b98
Ruslan <mask238@gmail.com>
2021-01-22 15:35:09 +0200

    Create order form

--- src/pages/Order/Order.jsx
+++ src/pages/Order/Order.jsx
@@ -18,6 +18,79 @@ class Order extends React.Component {
                         <div className={styles.productInfo}>
                             <h3 className={styles.name}>Ножницы парикмахерские со съемным упором</h3>
                             <p className={styles.description}>Некоторые продавцы ножниц громко заявляют о том, что их ножницы, мол, точат себя сами. Якобы одно лезвие сделано из более твердого </p>
+
+                            <form className={styles.orderForm}>
+                                <fieldset>
+                                    <label className={styles.type}>
+                                        <input className={styles.radio} type="radio" name="type" value="съемный упор"/>
+                                        <span className={styles.fakeRadio}>Съемный упор</span>
+                                    </label>
+                                    <label className={styles.type}>
+                                        <input className={styles.radio} type="radio" name="type" value="литой упор"/>
+                                        <span className={styles.fakeRadio}>Литой упор</span>
+                                    </label>
+                                </fieldset>
+
+                                <fieldset className={styles.prodColor}>
+                                    <legend className={styles.legend}>Цвет:</legend>
+                                    <label className={styles.color}>
+                                        <input className={styles.radio} type="radio" name="color" value="золотой"/>
+                                        <span className={`${styles.fakeRadio} ${styles.gold}`}></span>
+                                    </label>
+                                    <label className={styles.color}>
+                                        <input className={styles.radio} type="radio" name="color" value="серый"/>
+                                        <span className={`${styles.fakeRadio} ${styles.silver}`}></span>
+                                    </label>
+                                    <label className={styles.color}>
+                                        <input className={styles.radio} type="radio" name="color" value="черный"/>
+                                        <span className={`${styles.fakeRadio} ${styles.matSilver}`}></span>
+                                    </label>
+                                </fieldset>
+
+                                <fieldset className={styles.prodSize}>
+                                    <legend className={styles.legend}>Размер:</legend>
+                                    <label className={styles.size}>
+                                        <input className={styles.radio} type="radio" name="size" value="4.5см"/>
+                                        <span className={styles.fakeRadio}>4.5см</span>
+                                    </label>
+                                    <label className={styles.size}>
+                                        <input className={styles.radio} type="radio" name="size" value="5.0см"/>
+                                        <span className={styles.fakeRadio}>5.0см</span>
+                                    </label>
+                                    <label className={styles.size}>
+                                        <input className={styles.radio} type="radio" name="size" value="5.5см"/>
+                                        <span className={styles.fakeRadio}>5.5см</span>
+                                    </label>
+                                    <label className={styles.size}>
+                                        <input className={styles.radio} type="radio" name="size" value="6.0см"/>
+                                        <span className={styles.fakeRadio}>6.0см</span>
+                                    </label>
+                                    <label className={styles.size}>
+                                        <input className={styles.radio} type="radio" name="size" value="6.5см"/>
+                                        <span className={styles.fakeRadio}>6.5см</span>
+                                    </label>
+                                </fieldset>
+
+                                <fieldset className={styles.options}>
+                                    <label className={styles.option}>
+                                        <input className={styles.checkbox} type="checkbox" value="гравировка"/>
+                                        <span className={styles.fakeCheckbox}></span>
+                                        <span className={styles.text}>Хочу гравировку</span>
+                                    </label>
+                                    <label className={styles.option}>
+                                        <input className={styles.checkbox} type="checkbox" value="чехол"/>
+                                        <span className={styles.fakeCheckbox}></span>
+                                        <span className={styles.text}>Хочу чехол</span>
+                                    </label>
+                                </fieldset>
+                                
+
+                                <div className={styles.actionBlock}>
+                                    <span className={styles.price}>700 грн.</span>
+                                    {/* <input className={styles.submitBtn} type="submit" value="Заказать"/> */}
+                                    <button className={styles.submitBtn}>Заказать</button>
+                                </div>
+                            </form>
                         </div>
 
                     </div>
