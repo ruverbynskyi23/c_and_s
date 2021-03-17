7a318a0450fea2064fcbac56777578eb2e831cec
Ruslan <mask238@gmail.com>
2021-03-17 17:04:39 +0200

    Create page Coming-Soon

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Switch, Route, useLocation } from 'react-router-dom';
 import { Header, Contacts, Footer } from './components/index';
-import { Home, About, Gallery, Faq, Order, NotFound} from './pages/index';
+import { Home, About, Gallery, Faq, Order, NotFound, ComingSoon } from './pages/index';
 import './App.css';
 
 function App() {
@@ -26,6 +26,7 @@ function App() {
           <Route exact path="/gallery" component={Gallery}/>
           <Route exact path="/faqs" component={Faq}/>
           <Route exact path="/order" component={Order}/>
+          <Route exact path="/coming-soon" component={ComingSoon}/>
           <Route component={NotFound}/>
         </Switch>
       </main>
